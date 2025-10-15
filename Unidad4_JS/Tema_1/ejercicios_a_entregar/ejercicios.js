a6781c670253ce3825d9b3618ee7b5c3f0c24c90
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-10-15 20:15:12 +0200

    Ejercicios empezados

---
+++ Unidad4_JS/Tema_1/ejercicios_a_entregar/ejercicios.js
@@ -0,0 +1,57 @@
+// 1. Declara una variable con let que almacene tu edad y reasígnala con un nuevo valor.
+
+// let miEdad = 24
+
+// miEdad = 40
+
+// console.log(miEdad)
+
+// 2. Declara una constante const para representar el valor de la velocidad de la luz (299792458 m/s). Intenta reasignar su valor y observa qué ocurre.
+
+// const velocidadLuz = 299792458
+
+// velocidadLuz = 1
+
+// console.log(velocidadLuz)
+
+// No se puede reasignar un valor de una variable que es constante.
+
+// 3. Declara variables usando la notación camelCase para los siguientes conceptos: Intenta declarar una variable con un número al inicio de su nombre. ¿Qué ocurre?
+/*
+1. Tu nombre completo
+2. Tu color favorito
+3. Tu ciudad actual
+*/
+
+// let 1nombreCompleto = "Oscar Luque Porca" // No se puede declarar una variable que empiece con un número, ya que nos avisa de que no está permitido
+
+// let colorFav = "Rojo"
+
+// let ciudadActual = "Málaga"
+
+
+// 4.  Declara variables de los siguientes tipos y escribe un ejemplo para cada uno:
+// Número entero
+// Número decimal
+// Cadena (string)
+// Booleano
+// Null
+
+
+let numEntero = 20
+
+let numDecimal = 20.1
+
+let cadena = "Ejemplo"
+
+let boolean = false
+
+let variable = null
+
+// 5. Usa console.log() para imprimir el tipo de cada variable utilizando typeof .
+
+console.log(typeof(numEntero))
+console.log(typeof(numDecimal))
+console.log(typeof(cadena))
+console.log(typeof(boolean))
+console.log(typeof(variableNull))
