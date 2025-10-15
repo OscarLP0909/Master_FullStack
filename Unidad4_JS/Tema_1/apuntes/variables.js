454c69cd31884f57f6e64e27905f6a90ed022c6c
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-10-15 17:33:07 +0200

    Apuntes Tema 1 JS

---
+++ Unidad4_JS/Tema_1/apuntes/variables.js
@@ -0,0 +1,23 @@
+// let patata = 23
+
+// console.log(patata)
+
+// function prueba() {
+//     let patata = 25
+//     console.log(patata)
+// }
+
+// prueba()
+
+
+// Tipos de datos
+
+let int = 23 //Int
+
+let float = 23.1 //Float
+
+let string = "Hola" //String
+
+let string2 = "...." //String              Para JavaScript, un String es como un Array
+
+let string3 = "HOLA" // Para JavaScript no hay ninguna diferencia entres estos 3 Strings
