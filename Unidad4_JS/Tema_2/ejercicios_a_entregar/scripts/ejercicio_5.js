845e116d5123d7767168919be4a196b5a31d5a8d
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-10-16 23:31:18 +0200

    Ejercicio 5

---
+++ Unidad4_JS/Tema_2/ejercicios_a_entregar/scripts/ejercicio_5.js
@@ -0,0 +1,3 @@
+/*Pide al usuario que escriba un día de la semana (lunes, martes…). Mostrar en pantalla el día que dice que es si es entre lunes y
+viernes (Por ejemplo "Hoy es lunes") y si es fin de semana mostrar "Es fin de semana". Hazlo una vez usando switch y otra con if
+else.*/
