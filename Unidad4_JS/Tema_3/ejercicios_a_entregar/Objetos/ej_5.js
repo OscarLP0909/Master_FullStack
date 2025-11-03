5bc9fb385c47ffd560802432f019ef32412f1151
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-11-03 17:28:53 +0100

    Ejercicios Objetos del 4 al 6

---
+++ Unidad4_JS/Tema_3/ejercicios_a_entregar/Objetos/ej_5.js
@@ -0,0 +1,366 @@
+let letraNombre = {
+    a: {
+        letra: "a",
+        masculino: "La bestia",
+        femenino: "La bestia",
+    },
+    b: {
+        letra: "b",
+        masculino: "Agente",
+        femenino: "Agente",
+    },
+    c: {
+        letra: "c",
+        masculino: "Gigante",
+        femenino: "Gigante",
+    },
+    d: {
+        letra: "d",
+        masculino: "Pokemon",
+        femenino: "Pokemon",
+    },
+    e: {
+        letra: "e",
+        masculino: "Dragón",
+        femenino: "Dragón",
+    },
+    f: {
+        letra: "f",
+        masculino: "Alien",
+        femenino: "Alien",
+    },
+    g: {
+        letra: "g",
+        masculino: "Fantasma",
+        femenino: "Fantasma",
+    },
+    h: {
+        letra: "h",
+        masculino: "Doctor",
+        femenino: "Doctora",
+    },
+    i: {
+        letra: "i",
+        masculino: "Genio",
+        femenino: "Genia",
+    },
+    j: {
+        letra: "j",
+        masculino: "Chico",
+        femenino: "Chica",
+    },
+    k: {
+        letra: "k",
+        masculino: "Príncipe",
+        femenino: "Princesa",
+    },
+    l: {
+        letra: "l",
+        masculino: "Hombre",
+        femenino: "Mujer",
+    },
+    m: {
+        letra: "m",
+        masculino: "Gatito",
+        femenino: "Gatita",
+    },
+    n: {
+        letra: "n",
+        masculino: "Borracho",
+        femenino: "Borracha",
+    },
+    o: {
+        letra: "o",
+        masculino: "Rey",
+        femenino: "Reina",
+    },
+    p: {
+        letra: "p",
+        masculino: "Robot",
+        femenino: "Robot",
+    },
+    q: {
+        letra: "q",
+        masculino: "Científico",
+        femenino: "Científica",
+    },
+    r: {
+        letra: "r",
+        masculino: "Samurai",
+        femenino: "Samurai",
+    },
+    s: {
+        letra: "s",
+        masculino: "Mole",
+        femenino: "Mole",
+    },
+    t: {
+        letra: "t",
+        masculino: "Loco",
+        femenino: "Loca",
+    },
+    u: {
+        letra: "u",
+        masculino: "Vigilante",
+        femenino: "Vigilante",
+    },
+    v: {
+        letra: "v",
+        masculino: "Guerrero",
+        femenino: "Guerrera",
+    },
+    w: {
+        letra: "w",
+        masculino: "León",
+        femenino: "Leona",
+    },
+    x: {
+        letra: "x",
+        masculino: "Ángel",
+        femenino: "Ángel",
+    },
+    y: {
+        letra: "y",
+        masculino: "Tigre",
+        femenino: "Tigresa",
+    },
+    z: {
+        letra: "z",
+        masculino: "Saiyajin",
+        femenino: "Saiyajin",
+    },
+};
+
+let letraApellido = {
+    a: {
+        letra: "a",
+        masculino: "indestructible",
+        femenino: "indestructible",
+    },
+    b: {
+        letra: "b",
+        masculino: "maravilla",
+        femenino: "maravilla",
+    },
+    c: {
+        letra: "c",
+        masculino: "radiactivo",
+        femenino: "radiactiva",
+    },
+    d: {
+        letra: "d",
+        masculino: "volador",
+        femenino: "voladora",
+    },
+    e: {
+        letra: "e",
+        masculino: "mágico",
+        femenino: "mágica",
+    },
+    f: {
+        letra: "f",
+        masculino: "salvaje",
+        femenino: "salvaje",
+    },
+    g: {
+        letra: "g",
+        masculino: "vengador",
+        femenino: "vengadora",
+    },
+    h: {
+        letra: "h",
+        masculino: "letal",
+        femenino: "letal",
+    },
+    i: {
+        letra: "i",
+        masculino: "poderoso",
+        femenino: "poderosa",
+    },
+    j: {
+        letra: "j",
+        masculino: "amoroso",
+        femenino: "amorosa",
+    },
+    k: {
+        letra: "k",
+        masculino: "oscuro",
+        femenino: "oscura",
+    },
+    l: {
+        letra: "l",
+        masculino: "fantástico",
+        femenino: "fantástica",
+    },
+    m: {
+        letra: "m",
+        masculino: "invencible",
+        femenino: "invencible",
+    },
+    n: {
+        letra: "n",
+        masculino: "valiente",
+        femenino: "valiente",
+    },
+    o: {
+        letra: "o",
+        masculino: "superveloz",
+        femenino: "superveloz",
+    },
+    p: {
+        letra: "p",
+        masculino: "invisible",
+        femenino: "invisible",
+    },
+    q: {
+        letra: "q",
+        masculino: "mutante",
+        femenino: "mutante",
+    },
+    r: {
+        letra: "r",
+        masculino: "tóxico",
+        femenino: "tóxica",
+    },
+    s: {
+        letra: "s",
+        masculino: "sexy",
+        femenino: "sexy",
+    },
+    t: {
+        letra: "t",
+        masculino: "asesino",
+        femenino: "asesina",
+    },
+    u: {
+        letra: "u",
+        masculino: "karateka",
+        femenino: "karateka",
+    },
+    v: {
+        letra: "v",
+        masculino: "galáctico",
+        femenino: "galáctica",
+    },
+    w: {
+        letra: "w",
+        masculino: "carismático",
+        femenino: "carismática",
+    },
+    x: {
+        letra: "x",
+        masculino: "violento",
+        femenino: "violenta",
+    },
+    y: {
+        letra: "y",
+        masculino: "nocturno",
+        femenino: "nocturna",
+    },
+    z: {
+        letra: "z",
+        masculino: "venenoso",
+        femenino: "venenosa",
+    },
+};
+
+let fechaNacimiento = {
+    num0: {
+        numero: 0,
+        masculino: "del espacio",
+        femenino: "del espacio",
+    },
+    num1: {
+        numero: 1,
+        masculino: "de la justicia",
+        femenino: "de la justicia",
+    },
+    num2: {
+        numero: 2,
+        masculino: "de Marvel",
+        femenino: "de Marvel",
+    },
+    num3: {
+        numero: 3,
+        masculino: "de las tinieblas",
+        femenino: "de las tinieblas",
+    },
+    num4: {
+        numero: 4,
+        masculino: "de fuego",
+        femenino: "de fuego",
+    },
+    num5: {
+        numero: 5,
+        masculino: "de la noche",
+        femenino: "de la noche",
+    },
+    num6: {
+        numero: 6,
+        masculino: "del futuro",
+        femenino: "del futuro",
+    },
+    num7: {
+        numero: 7,
+        masculino: "de acero",
+        femenino: "de acero",
+    },
+    num8: {
+        numero: 8,
+        masculino: "exterminador",
+        femenino: "exterminadora",
+    },
+    num9: {
+        numero: 9,
+        masculino: "de otro planeta",
+        femenino: "de otro planeta",
+    },
+};
+
+// 5. Generador de nombres de superhéroe:
+// Pide al usuario su fecha de nacimiento, nombre y apellido.
+// Genera el nombre de superhéroe según:
+// Primera letra del nombre → propiedad del objeto letraNombre .
+// Última letra del apellido → propiedad del objeto letraApellido .
+// Último número del año de nacimiento → propiedad del objeto fechaNacimiento .
+// Pregunta si quiere el nombre masculino o femenino. Muestra el nombre generado.
+
+
+// Pedimos el nombre, apellido y género
+
+let nombre = window.prompt(`Introduzca su nombre: `)
+
+let primeraLetra = nombre[0]
+
+let apellido = window.prompt("Introduzca su apellido:")
+
+let ultimaLetra = apellido[apellido.length - 1]
+
+let fecha = window.prompt(`Introduzca año de nacimiento: `)
+
+let ultimaFecha = fecha[fecha.length - 1]
+
+let genero = prompt("¿Desea nombre masculino o femenino? (escriba 'masculino' o 'femenino')")
+    .trim()
+    .toLowerCase();
+
+console.log("Género introducido:", genero);
+console.log("Tipo de dato:", typeof genero);
+console.log("Valor en letraApellido[ultimaLetra]:", letraApellido[ultimaLetra]);
+
+
+// Buscamos en el objeto lo que cumpla con lo que hemos añadido
+
+let parte1 = letraNombre[primeraLetra][genero];
+console.log(parte1)
+let parte2 = letraApellido[ultimaLetra][genero];
+console.log(parte2)
+let parte3 = fechaNacimiento[`num${ultimaFecha}`][genero];
+console.log(parte3)
+
+// Mostramos el resultado
+
+let superheroe = `${parte1} ${parte2} ${parte3}`;
+
+window.alert(`Tu nombre de superheroe es: ${superheroe}`)
+
