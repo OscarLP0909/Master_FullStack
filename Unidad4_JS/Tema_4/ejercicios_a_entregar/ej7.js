d72584af8bb4b6891cc27cd6096d2cb8dfbe5d14
OscarLP09 <oscar.s352656@cesurformacion.com>
2025-11-10 19:32:13 +0100

    Ejercicio 7 Listo

--- Unidad4_JS/Tema_4/ejercicios_a_entregar/ej7.js
+++ Unidad4_JS/Tema_4/ejercicios_a_entregar/ej7.js
@@ -1,6 +1,15 @@
 // Diseña una función esPar() que reciba como parámetro un número y devuelva si ese número es par o no. Utilízalo en un
 // programa que lea un número por prompt y determine si es par o no y muestre un mensaje al respecto en un alert.
 
-function esPar(numero) {
-    return numero % 2 === 0
+let numero = Number(window.prompt(`Escriba el número: `))
+
+function esPar(a) {
+    if(a % 2 == 0) {
+        alert(`El ${numero} es par`)
+        return true
+    } else {
+        alert(`El ${numero} no es par`)
+    }
 }
+
+const resultado = esPar(numero)
