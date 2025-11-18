c5a3d638964eaa46d9622abc522eaeb78424154e
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-11-18 12:47:36 +0100

    Ejercicio 1 Eventos realizado

--- Unidad4_JS/Tema_6_Eventos/apuntes/script.js
+++ Unidad4_JS/Tema_6_Eventos/apuntes/script.js
@@ -1,21 +1,33 @@
 // A través del document
-let abuelo = document.getElementById("odin")
-let padre = document.getElementById("paternoster")
-let nieto = document.getElementById("boton")
+// let abuelo = document.getElementById("odin")
+// let padre = document.getElementById("paternoster")
+// let nieto = document.getElementById("boton")
 
 
-abuelo.addEventListener("click", (e)=>{imprimir(e)})
-abuelo.addEventListener("click", (e)=>{console.log("Abuelo")})
+// abuelo.addEventListener("click", (e)=>{imprimir(e)})
+// abuelo.addEventListener("click", (e)=>{console.log("Abuelo")})
 
-padre.addEventListener("click", (e)=>{imprimir(e)})
-padre.addEventListener("click", (e)=>{console.log("Padre")})
+// padre.addEventListener("click", (e)=>{imprimir(e)})
+// padre.addEventListener("click", (e)=>{console.log("Padre")})
 
-nieto.addEventListener("click", (e)=>{imprimir(e)})
-nieto.addEventListener("click", (e)=>{console.log("Nieto")})
+// nieto.addEventListener("click", (e)=>{imprimir(e)})
+// nieto.addEventListener("click", (e)=>{console.log("Nieto")})
 
 
 // A través de argumentos (solo podemos tener uno)
-function imprimir(e) {
-    console.log("He sido pulsado")
-    console.log(e)
-}
+// function imprimir(e) {
+//     console.log("He sido pulsado")
+//     console.log(e)
+// }
+
+// Cambiar el color de fondo mediante el click de un boton
+
+let boton = document.getElementById("boton")
+boton.addEventListener("click", () => {
+    let colorActual = document.getElementById("odin").style.backgroundColor
+    if(colorActual === "green") {
+        document.getElementById("odin").style.backgroundColor = "red"
+    } else {
+        document.getElementById("odin").style.backgroundColor = "green"
+    }
+})
