7eb40bf436e7860246beb61240c1764c898db81f
OscarLP0909 <oscar.s352656@cesurformacion.com>
2025-12-02 11:04:34 +0100

    Comentado el script

--- Unidad4_JS/Tema_7_AJAX/pruebas/script.js
+++ Unidad4_JS/Tema_7_AJAX/pruebas/script.js
@@ -15,6 +15,7 @@ const containerDropdown = document.createElement("div")
 const divInfo = document.createElement("div")
 divInfo.id = "divInfo"
 
+// DropDown con los tipos de los Pokemon
 fetch(`https://pokeapi.co/api/v2/type`)
     .then((res) => res.json())
     .then(data => {
@@ -29,15 +30,20 @@ fetch(`https://pokeapi.co/api/v2/type`)
         document.body.appendChild(containerDropdown)
         document.body.appendChild(divInfo)
 
+        // Evento cada vez que cambiemos de tipo en el dropdown
         select.addEventListener("change", () => {
             divInfo.innerHTML = "";
             const tipoSeleccionado = data.results.find(tipo => tipo.name === select.value)
             console.log(tipoSeleccionado)
+
+            // Hacemos Fetch sobre el tipo seleccionado y obtenemos la url de los pokemon
             fetch(tipoSeleccionado.url)
                 .then(res => res.json())
                 .then(tipoCompleto => {
                     console.log(tipoCompleto.pokemon)
                     tipoCompleto.pokemon.forEach(p => {
+
+                        // Hacemos otro fetch para obtener otra url donde se encuentra toda la informacion del pokemon
                         fetch(p.pokemon.url)
                             .then(res => res.json())
                             .then(infoCompleta => {
