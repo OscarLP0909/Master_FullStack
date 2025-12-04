0480a81ee80c9912548bee40bd0dc029013842b7
OscarLP09 <oscar.s352656@cesurformacion.com>
2025-12-04 21:16:26 +0100

    Mitad de la clase 1/8 TS

---
+++ Unidad5_TS/apuntes/index.ts
@@ -0,0 +1,15 @@
+(() => {
+    const myName = "Oscar"
+
+    //El hover de myName sale que es de tipo "Oscar" en vez de string
+    // por ser const
+
+    // *Inferencia de tipos
+
+    let age = 35
+
+    // Con let, el hover muestra que es de tipo number
+
+    // Redeclaración y reasignación
+})();
+
