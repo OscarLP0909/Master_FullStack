0fb767050d5db1bb44a6e281853e0955db576334
OscarLP09 <oscar.s352656@cesurformacion.com>
2025-12-05 17:36:30 +0100

    Clase 1 Terminada

--- Unidad5_TS/apuntes/index.js
+++ Unidad5_TS/apuntes/index.js
@@ -1,11 +1,53 @@
 "use strict";
-(() => {
-    const myName = "Oscar";
-    //El hover de myName sale que es de tipo "Oscar" en vez de string
-    // por ser const
-    // *Inferencia de tipos
-    let age = 35;
-    // Con let, el hover muestra que es de tipo number
-    // Redeclaración y reasignación
-})();
-//# sourceMappingURL=index.js.map
+const myName = "Oscar";
+//El hover de myName sale que es de tipo "Oscar" en vez de string
+// por ser const
+// *Inferencia de tipos
+let age = 35;
+// Con let, el hover muestra que es de tipo number
+age = 36;
+//* Boolean
+let isSummer = false;
+isSummer = true;
+//* Arrays
+const months = ["enero", "febrero", "marzo"];
+months.push("junio");
+//* Funciones
+function sayHello(nombre) {
+    return `Hola ${nombre}`;
+}
+function addNumber(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumber(3, 5)); // 8
+//console.log(addNumber(3, "15")) // 315
+//console.log(addNumber(15)) // NaN
+function addNumberOrNot(num1, num2) {
+    if (num1 > 5) {
+        return num1 + num2;
+    }
+}
+const addNumbersArrowFn = (num1, num2) => num1 + num2;
+const toUpperCase = (palabra) => palabra.toUpperCase();
+console.log(addNumberOrNot(1, 3));
+console.log(sayHello("Oscar"));
+function logMessage(message) {
+    // *Type guards
+    //* instanceof
+    if (typeof message === 'string') {
+        return message.toUpperCase();
+    }
+    else {
+        return message.toFixed(2);
+    }
+}
+logMessage(123);
+logMessage("Hola");
+//* Type any implícito
+let msg;
+function fnWithAny(param1) {
+    console.log(param1);
+}
+//* Función sin excplicitar tipos
+const doubles = [1, 3, 5, 7].map((numero) => numero * 2);
+console.log(doubles);
--- Unidad5_TS/apuntes/index.ts
+++ Unidad5_TS/apuntes/index.ts
@@ -1,4 +1,4 @@
-(() => {
+
     const myName = "Oscar"
 
     //El hover de myName sale que es de tipo "Oscar" en vez de string
@@ -6,10 +6,71 @@
 
     // *Inferencia de tipos
 
-    let age = 35
+    let age: number = 35
 
     // Con let, el hover muestra que es de tipo number
 
-    // Redeclaración y reasignación
-})();
+    age = 36
 
+    //* Boolean
+    let isSummer: boolean = false;
+    isSummer = true;
+
+    //* Arrays
+    const months: string[] = ["enero", "febrero", "marzo"];
+    months.push("junio")
+
+    //* Funciones
+    function sayHello(nombre: string): string {
+        return `Hola ${nombre}`;
+    }
+
+    function addNumber(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+
+    console.log(addNumber(3, 5)) // 8
+    //console.log(addNumber(3, "15")) // 315
+    //console.log(addNumber(15)) // NaN
+
+    function addNumberOrNot(num1: number, num2: number): number | undefined {
+        if (num1 > 5) {
+            return num1 + num2
+        }
+    }
+
+    const addNumbersArrowFn = (num1: number, num2: number): number => num1 + num2
+
+    const toUpperCase = (palabra: string) => palabra.toUpperCase();
+
+    console.log(addNumberOrNot(1, 3));
+
+    console.log(sayHello("Oscar"));
+
+
+    //* Type Alias
+    type stringOrNumber = string | number;
+
+    function logMessage (message: stringOrNumber): stringOrNumber {
+        // *Type guards
+        //* instanceof
+        if (typeof message === 'string'){
+            return message.toUpperCase();
+        } else {
+            return message.toFixed(2);
+        }
+    }
+
+    logMessage(123)
+    logMessage("Hola")
+
+    //* Type any implícito
+    let msg: string;
+
+    function fnWithAny(param1: string) {
+        console.log(param1)
+    }
+
+    //* Función sin excplicitar tipos
+    const doubles = [1, 3, 5, 7].map((numero: number) => numero * 2);
+    console.log(doubles);
