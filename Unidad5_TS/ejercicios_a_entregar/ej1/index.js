f8a20ba0654cdd5409d0a69e4e74fba35fabcc33
OscarLP09 <oscar.s352656@cesurformacion.com>
2025-12-07 00:05:37 +0100

    organizado ej1

--- Unidad5_TS/ejercicios_a_entregar/ej1/index.js
+++ Unidad5_TS/ejercicios_a_entregar/ej1/index.js
@@ -1,7 +1,7 @@
 "use strict";
 const boton = document.getElementById("boton");
 boton === null || boton === void 0 ? void 0 : boton.addEventListener("click", (Event) => {
-    Event.preventDefault();
+    Event.preventDefault(); // esto sirve para que cuando se le dé a enviar, no mande nada
     const nameForm = document.getElementById("nombre");
     function mostrarInfoForm(nombre) {
         console.log(nombre);
--- Unidad5_TS/ejercicios_a_entregar/ej1/index.ts
+++ Unidad5_TS/ejercicios_a_entregar/ej1/index.ts
@@ -2,7 +2,7 @@ const boton = document.getElementById("boton")
 
 
 boton?.addEventListener("click", (Event) => {
-    Event.preventDefault()
+    Event.preventDefault() // esto sirve para que cuando se le dé a enviar, no mande nada
     const nameForm = document.getElementById("nombre") as HTMLInputElement
     function mostrarInfoForm(nombre: string): void {
         console.log(nombre)
